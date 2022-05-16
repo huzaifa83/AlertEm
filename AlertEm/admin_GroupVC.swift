59b6ac306db35c6389c3434250e1181f526e35cd
Mithilesh Mishra <mishramithilesh94@gmail.com>
2022-05-16 16:31:40 +0530

    added  3 view controllers after OkayVC

---
+++ AlertEm/admin_GroupVC.swift
@@ -0,0 +1,29 @@
+//
+//  admin_GroupVC.swift
+//  AlertEm
+//
+//  Created by Mithilesh Mishra on 16/05/22.
+//
+
+import UIKit
+
+class admin_GroupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
